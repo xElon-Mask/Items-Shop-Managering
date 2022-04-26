cebffafe1ad09bb5f519841c9a6fd3a5c6ba2fa4
xElon-Mask <xelonmask@protonmail.com>
2022-04-26 21:42:58 +0200

    add struct Item and create array with this struct

--- ItemsShopManager.sol
+++ ItemsShopManager.sol
@@ -3,5 +3,12 @@
 pragma solidity 0.8.13;
 
 contract ItemsShopManager {
-    
+
+    struct Item {
+        string name;
+        uint price;
+        uint units;
+    }
+
+    Item[] public items;
 }
